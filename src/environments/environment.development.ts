8f6e35a471b65897f433ac2141cee8b3ba4b15c9
Arack <arack.rs@gmail.com>
2025-05-09 11:40:35 -0500

    fix(auth): add continue with google

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,4 +2,5 @@ export const environment = {
   production: false,
   apiUrl: 'http://localhost:8080',
   googleClientId: '353060260533-ol23f81fr8pqo3a8ibjqprrv6kgpd4bf.apps.googleusercontent.com',
+  frontendUrl: 'http://localhost:4200',
 };
